77be72a1137ff90ce42e5bd2eb0cdf9c32695163
oleg aleynikov <aleynikov@raduga-lik.ru>
2024-04-17 11:20:08 +0300

    float

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,6 +9,45 @@ namespace ConsoleApp1
             //var a1 = useRegex("#1234-abc de");
             //var a2 = useRegex("#1");
             //var a3 = useRegex("1, Red House, 2551");
+            var false1 = RegularExpression.UseRegex("-1"); // floatRange
+            var false2 = RegularExpression.UseRegex("-0.1"); // floatRange
+            
+            var double_true_1 = RegularExpression.UseRegex("0.0"); // floatRange
+            var double_true_3 = RegularExpression.UseRegex("0.1"); // floatRange
+            var double_true_4 = RegularExpression.UseRegex("0.2"); // floatRange
+            var double_true_5 = RegularExpression.UseRegex("5.5"); // floatRange
+            var double_true_6 = RegularExpression.UseRegex("9.9"); // floatRange
+            var double_true_7 = RegularExpression.UseRegex("10"); // floatRange
+
+            var comma_true_1 = RegularExpression.UseRegex("0,0"); // floatRange
+            var comma_true_2 = RegularExpression.UseRegex("0,1"); // floatRange
+            var comma_false_1 = RegularExpression.UseRegex("00,0"); // floatRange
+            
+            var wrong_delimeter_false_1 = RegularExpression.UseRegex("0`0"); // floatRange
+            var wrong_delimeter_false_2 = RegularExpression.UseRegex("0 0"); // floatRange
+
+            var double_true_8 = RegularExpression.UseRegex("10.0"); // floatRange
+            var double_false_9 = RegularExpression.UseRegex("10.1"); // floatRange
+            
+            var double_false_10 = RegularExpression.UseRegex("11.1"); // floatRange
+            var double_false_11 = RegularExpression.UseRegex("11"); // floatRange
+            var double_false_12 = RegularExpression.UseRegex("5.05"); // floatRange
+            var double_false_13 = RegularExpression.UseRegex("05.5"); // floatRange
+            var double_false_14 = RegularExpression.UseRegex("110"); // floatRange
+            var double_false_15 = RegularExpression.UseRegex("111"); // floatRange
+
+            var true1 = RegularExpression.UseRegex("0"); // floatRange
+            var true_9 = RegularExpression.UseRegex("12HELL0111(123)"); // floatRange
+            var true_10 = RegularExpression.UseRegex("10.0"); // floatRange
+            var true13 = RegularExpression.UseRegex("1"); // floatRange
+            var true14 = RegularExpression.UseRegex("2"); // floatRange
+            //var true15 = RegularExpression.UseRegex("5 f 9"); // floatRange
+            //var true16 = RegularExpression.UseRegex("60"); // floatRange
+            //var true17 = RegularExpression.UseRegex("a"); // floatRange
+            //var true18 = RegularExpression.UseRegex("a1"); // floatRange
+
+            //var a1 = RegularExpression.UseRegex("45");
+
             Console.WriteLine("Hello World!");
         }
     }
--- ConsoleApp1/RegularExpression.cs
+++ ConsoleApp1/RegularExpression.cs
@@ -12,6 +12,7 @@ namespace ConsoleApp1
 
         /// <summary>
         /// Test
+        /// ^...$ - начало/конец строки
         /// </summary>
         public static bool UseRegex(string input)
         {
@@ -22,7 +23,22 @@ namespace ConsoleApp1
             //Regex regex = new Regex("^#[0-9]+$", RegexOptions.IgnoreCase);
             //Regex regex = new Regex("(^[0-9]+$)", RegexOptions.IgnoreCase);
             //var regex = new Regex(@"([a-zA-Z\s]*), ([a-zA-Z\s]*), ([0-9]{4})");
-            var regex = new Regex(@"#([0-9]{4})-([a-z, ,0-9]{5,10}$)");
+            //var regex = new Regex(@"#([0-9]{4})-([a-z, ,0-9]{5,10}$)");
+            //var regex = new Regex(@"^[0.1-10.0]*$"); // float range
+            //var regex = new Regex(@"^\d{1}(?:\.\d{0,1})|10|10.0?$"); // 0...10 float range
+
+            //var regex = new Regex(@"^(\d{1}(?:\.\d{1}))|([1]{1}[0]{1})$"); // 0...10 float range
+
+            //var regex = new Regex(@"^\d{1}(?:(\.|\,)\d{1})|[1]{1}[0]{1}$"); // 0...10 float range
+
+            //var regex = new Regex(@"HELLO"); // float range
+            //var regex = new Regex(@"[1]{1}[0]{1}\.[0]{1}"); // 10.0
+
+            //var regex = new Regex(@"^[0-5]?[0-9]$"); // 0..59
+            //var regex = new Regex(@"^\d{1}$"); // 0..9
+            //var regex = new Regex(@"[0-9]|[0]");
+            //var regex = new Regex(@"\d{3}");
+
             return regex.IsMatch(input);
         }
     }
